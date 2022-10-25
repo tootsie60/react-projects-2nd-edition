b6ebf33a7eb0027fa123f91e00b987c9f3721000
tootsie60 <73935697+tootsie60@users.noreply.github.com>
2022-10-25 15:31:32 -0700

    First commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+    return <h1>Rick and Morty</h1>;
+}
+
+const container = document.getElementById('app');
+const root = ReactDOM.createRoot(container);
+
+root.render(<App />);
